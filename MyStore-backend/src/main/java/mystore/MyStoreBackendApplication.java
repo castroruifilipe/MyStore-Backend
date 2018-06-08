660a776f67c8c99f8291b6813b8e43bd422ff747
Rui Leite <a75551@alunos.uminho.pt>
2018-06-08 12:02:40 +0100

    Excecoes

--- MyStore-backend/src/main/java/mystore/MyStoreBackendApplication.java
+++ MyStore-backend/src/main/java/mystore/MyStoreBackendApplication.java
@@ -8,10 +8,31 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MyStoreBackendApplication {
+
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 
     @Bean
     public BCryptPasswordEncoder getPasswordEncoder() {
--- MyStore-backend/src/main/java/mystore/exceptions/ApiExceptionHandler.java
+++ MyStore-backend/src/main/java/mystore/exceptions/ApiExceptionHandler.java
@@ -1,7 +1,9 @@
 package mystore.exceptions;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -14,6 +16,7 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    @ResponseStatus(FORBIDDEN)
     @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
     public final ApiErrorResponse handleUserNotFoundException(Exception exception, WebRequest request) {
         ApiErrorResponse errorResponse = ApiErrorResponseBuilder.anApiErrorResponse()
@@ -24,7 +27,8 @@ public class ApiExceptionHandler {
         return errorResponse;
     }
 
-    @ExceptionHandler({IllegalArgumentException.class, EntityExistsException.class})
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler({IllegalArgumentException.class, EntityExistsException.class, Exception.class})
     public final ApiErrorResponse handleIllegalArgumentException(Exception exception, WebRequest request) {
         ApiErrorResponse errorResponse = ApiErrorResponseBuilder.anApiErrorResponse()
                 .withStatus(BAD_REQUEST)
