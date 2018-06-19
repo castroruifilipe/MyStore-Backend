b88eddc19f0a16265d6870e4e5f77460575eace4
Rui Leite <a75551@alunos.uminho.pt>
2018-06-19 16:53:10 +0100

    Encomendas

--- MyStore-backend/src/main/java/mystore/daos/EncomendaDAOImpl.java
+++ MyStore-backend/src/main/java/mystore/daos/EncomendaDAOImpl.java
@@ -11,14 +11,5 @@ import java.util.List;
 
 @Repository("encomendaDAO")
 public class EncomendaDAOImpl extends GenericDAOImpl<Encomenda, Long> implements EncomendaDAO {
-
-    @Override
-    public List<Encomenda> getAll() {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Encomenda> criteriaQuery = criteriaBuilder.createQuery(type);
-        Root<Encomenda> root = criteriaQuery.from(type);
-        root.fetch("cliente", JoinType.INNER);
-        criteriaQuery.select(root);
-        return entityManager.createQuery(criteriaQuery).getResultList();
-    }
+    
 }
