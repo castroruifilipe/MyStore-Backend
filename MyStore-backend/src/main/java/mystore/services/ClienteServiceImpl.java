fd24c018366d699c9f0581bd8ad0bae1580cb26d
Rui Leite <a75551@alunos.uminho.pt>
2018-06-27 14:22:17 +0100

    Initialize

--- MyStore-backend/src/main/java/mystore/services/ClienteServiceImpl.java
+++ MyStore-backend/src/main/java/mystore/services/ClienteServiceImpl.java
@@ -2,6 +2,7 @@ package mystore.services;
 
 import mystore.daos.ClienteDAO;
 import mystore.models.Cliente;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,8 @@ public class ClienteServiceImpl implements ClienteService {
     @Override
     @Transactional
     public Optional<Cliente> get(long uid) {
+        Optional<Cliente> optionalCliente = clienteDAO.find(uid);
+        optionalCliente.ifPresent(cliente -> Hibernate.initialize(cliente.getEncomendas()));
         return clienteDAO.find(uid);
     }
 
